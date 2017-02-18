0d01fa7678c8292ed8e18aab1350fc8f7911361a
Ric Wright <rkwright@geofx.com>
2017-02-18 10:18:50 -0600

    Initial commit of backup

---
+++ slideshow-2014/scripts/slideshow.js
@@ -0,0 +1,41 @@
+// JavaScript Document
+
+/*** 
+    Simple jQuery Slideshow Script
+    Released by Jon Raasch (jonraasch.com) under FreeBSD license: free to use or modify, not responsible for anything, etc.  Please link out to me if you like it :)
+***/
+
+/** this will work with any number of images 
+    set the active class on whichever image you want to show up as the default 
+    (otherwise this will be the last image) 
+
+    Note that you can add an <a> element to any of the images and allow the user to navigate away to    wherever
+	**/
+
+function slideSwitch() {
+    var $active = $('#slideshow DIV.active');
+
+    if ( $active.length == 0 ) $active = $('#slideshow DIV:last');
+
+    // use this to pull the divs in the order they appear in the markup
+    var $next =  $active.next().length ? $active.next()
+        : $('#slideshow DIV:first');
+
+    // uncomment below to pull the divs randomly
+    // var $sibs  = $active.siblings();
+    // var rndNum = Math.floor(Math.random() * $sibs.length );
+    // var $next  = $( $sibs[ rndNum ] );
+
+
+    $active.addClass('last-active');
+
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+}
+
+$(function() {
+    setInterval( "slideSwitch()", 3000 );
+});
